ba37b3c6baf2058743c4678d4b711500e986a13d
Saad Quadri <saad@saadquadri.com>
2017-05-06 19:02:13 -0400

    move template2 files/folders around

--- app/server/src/generator/templates/index.js
+++ app/server/src/generator/templates/index.js
@@ -1,13 +1,35 @@
+const { join } = require('path')
 const template1 = require('./template1')
 const template2 = require('./template2')
 const template3 = require('./template3')
+const { TEMPLATE1, TEMPLATE2, TEMPLATE3 } = require('../constants')
 
-function generateTemplate(data) {
+function getTemplateData(data) {
   switch (data.selectedTemplate) {
-    case 1: return template1(data)
-    case 2: return template2(data)
-    case 3: return template3(data)
+    case TEMPLATE1:
+      return {
+        texDoc: template1(data),
+        opts: {}
+      }
+
+    case TEMPLATE2:
+      console.log(template2(data))
+      return {
+        texDoc: template2(data),
+        opts: {
+          cmd: 'xelatex',
+          inputs: join(__dirname, 'template2', 'inputs'),
+          fonts: join(__dirname, 'template2', 'inputs'),
+          errorLogs: join(__dirname, 'latexerrors.log')
+        }
+      }
+
+    case TEMPLATE3:
+      return {
+        texDoc: template3(data),
+        opts: {}
+      }
   }
 }
 
-module.exports = generateTemplate
+module.exports = getTemplateData

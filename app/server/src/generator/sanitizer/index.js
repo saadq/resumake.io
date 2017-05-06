7db18f80d94bf584e3b656e25d20b477adfeabb7
Saad Quadri <saad@saadquadri.com>
2017-05-06 19:52:08 -0400

    add jsdoc comments to sanitizer

--- app/server/src/generator/sanitizer/index.js
+++ app/server/src/generator/sanitizer/index.js
@@ -1,5 +1,13 @@
 const sanitizeLatex = require('sanitize-latex')
 
+/**
+ * Recursively sanitizes an object by removing empty values,
+ * and normalizing strings with LaTeX symbols.
+ *
+ * @param {Object} obj - The object to be sanitized.
+ *
+ * @return {Object} - A sanitized copy of the input object.
+ */
 function sanitize(obj = {}) {
   if (Array.isArray(obj)) {
     return obj
@@ -34,10 +42,25 @@ function sanitize(obj = {}) {
   return copy
 }
 
+/**
+ * Checks to see if the value is NaN, null, undefined,
+ * an empty string, or an empty array/object.
+ *
+ * @param {any} val - The value to be checked.
+ *
+ * @return {boolean}
+ */
 function isEmpty(val) {
   return val == null || Number.isNaN(val) || isEmptyObject(val) || isEmptyString(val)
 }
 
+/**
+ * Checks to see if an object or an array is empty.
+ *
+ * @param {Object|Array} val - The object to be checked.
+ *
+ * @return {boolean}
+ */
 function isEmptyObject(val) {
   if (!isObject(val)) {
     return false
@@ -48,22 +71,58 @@ function isEmptyObject(val) {
     : Object.keys(val).length === 0
 }
 
+/**
+ * Checks to see if the value is an empty string or a string with just whitespace.
+ *
+ * @param {string} val - The value to be checked.
+ *
+ * @return {boolean}
+ */
 function isEmptyString(val) {
   return isString(val) && isOnlyWhitespace(val)
 }
 
+/**
+ * Checks to see if a value is a object (excluding null).
+ *
+ * @param {any} val - The value to be checked.
+ *
+ * @return {boolean}
+ */
 function isObject(val) {
   return val && typeof val === 'object'
 }
 
+/**
+ * [isString description]
+ *
+ * @param {[type]} val [description]
+ *
+ * @return {Boolean} [description]
+ */
 function isString(val) {
   return typeof val === 'string'
 }
 
+/**
+ * Checks to see if a string only contains whitespace.
+ *
+ * @param {string} str - The string to be checked.
+ *
+ * @return {boolean}
+ */
 function isOnlyWhitespace(str) {
   return !(/\S/).test(str.trim())
 }
 
+/**
+ * Converts multiple spaces in a string to a single space,
+ * and removes leading and traililng whitespace in the string.
+ *
+ * @param {string} str - The string to be trimmed
+ *
+ * @return {string} - The trimmed string.
+ */
 function trim(str) {
   return str.trim().replace(/\s\s+/g, ' ')
 }

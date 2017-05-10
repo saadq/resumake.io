3e457834a4d7a6846b46eb9c734dee3626f49e82
Saad Quadri <saad@saadquadri.com>
2017-05-10 12:05:44 -0400

    don't log redux-form actions

--- app/client/src/store/middleware.js
+++ app/client/src/store/middleware.js
@@ -1,13 +1,15 @@
 import { applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 
 const middleware = []
 
 if (process.env.NODE_ENV === 'development') {
   middleware.push(thunk)
-  middleware.push(logger)
+  middleware.push(createLogger({
+    predicate: (getState, action) => !action.type.startsWith('@@redux-form')
+  }))
 }
 
 export default composeWithDevTools(

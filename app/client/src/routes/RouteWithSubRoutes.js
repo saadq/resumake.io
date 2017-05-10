dbf78189debe1eee3b4ab5ad368731063b048e6b
Saad Quadri <saad@saadquadri.com>
2017-05-10 11:52:52 -0400

    change routes to use function declaration

--- app/client/src/routes/RouteWithSubRoutes.js
+++ app/client/src/routes/RouteWithSubRoutes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-const RouteWithSubRoutes = route => (
-  <Route
-    exact={route.path === '/'}
-    path={route.path}
-    render={props => <route.component {...props} routes={route.routes} />}
-  />
-)
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      exact={route.path === '/'}
+      path={route.path}
+      render={props => <route.component {...props} routes={route.routes} />}
+    />
+  )
+}
 
 export default RouteWithSubRoutes
--- app/client/src/routes/index.js
+++ app/client/src/routes/index.js
@@ -3,14 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 import RouteWithSubRoutes from './RouteWithSubRoutes'
 import routeConfig from './config'
 
-const Router = () => (
-  <BrowserRouter>
-    <div>
-      {routeConfig.map((route, i) => (
-        <RouteWithSubRoutes key={i} {...route} />
-      ))}
-    </div>
-  </BrowserRouter>
-)
+function Router() {
+  return (
+    <BrowserRouter>
+      <div>
+        {routeConfig.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
+      </div>
+    </BrowserRouter>
+  )
+}
 
 export default Router

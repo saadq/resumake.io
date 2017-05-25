20a4ca5786eeb6568877b690807d40c8b162be95
Saad Quadri <saad@saadquadri.com>
2017-05-25 07:52:19 -0400

    separate form reducer from redux-form for testing purposes

--- app/client/src/reducers/form.js
+++ app/client/src/reducers/form.js
@@ -7,112 +7,111 @@ import {
   CLEAR_SKILL_FIELD
 } from '../constants'
 
-const form = reducer.plugin({
-  resume: (state, action) => {
-    switch (action.type) {
-      case CLEAR_SCHOOL_FIELD:
-        if (
-          !state.values ||
-          !state.values.schools ||
-          state.values.schools.length <= 1 ||
-          state.values.schools.length !== action.schoolCount
-        ) {
-          return state
-        }
-
-        return {
-          ...state,
-          values: {
-            ...state.values,
-            schools: state.values.schools.slice(0, -1)
-          }
-        }
-
-      case CLEAR_JOB_FIELD:
-        if (
-          !state.values ||
-          !state.values.jobs ||
-          state.values.jobs.length <= 1 ||
-          state.values.jobs.length !== action.jobCount
-        ) {
-          return state
-        }
-
-        return {
-          ...state,
-          values: {
-            ...state.values,
-            jobs: state.values.jobs.slice(0, -1)
-          }
-        }
-
-      case CLEAR_JOB_DUTY_FIELD:
-        if (
-          !state.values ||
-          !state.values.jobs ||
-          !state.values.jobs[action.index] ||
-          !state.values.jobs[action.index].duties ||
-          state.values.jobs[action.index].duties.length <= 1 ||
-          action.jobDutyCount !== state.values.jobs[action.index].duties.length
-        ) {
-          return state
-        }
-
-        return {
-          ...state,
-          values: {
-            ...state.values,
-            jobs: [
-              ...state.values.jobs.slice(0, action.index),
-              {
-                ...state.values.jobs[action.index],
-                duties: state.values.jobs[action.index].duties.slice(0, -1)
-              },
-              ...state.values.jobs.slice(action.index + 1)
-            ]
-          }
-        }
-
-      case CLEAR_PROJECT_FIELD:
-        if (
-          !state.values ||
-          !state.values.projects ||
-          state.values.projects.length <= 1 ||
-          state.values.projects.length !== action.projectCount
-        ) {
-          return state
-        }
-
-        return {
-          ...state,
-          values: {
-            ...state.values,
-            projects: state.values.projects.slice(0, -1)
-          }
-        }
-
-      case CLEAR_SKILL_FIELD:
-        if (
-          !state.values ||
-          !state.values.skills ||
-          state.values.skills.length <= 1 ||
-          state.values.skills.length !== action.skillCount
-        ) {
-          return state
-        }
-
-        return {
-          ...state,
-          values: {
-            ...state.values,
-            skills: state.values.skills.slice(0, -1)
-          }
-        }
-
-      default:
+function form(state = {}, action) {
+  switch (action.type) {
+    case CLEAR_SCHOOL_FIELD:
+      if (
+        !state.values ||
+        !state.values.schools ||
+        state.values.schools.length <= 1 ||
+        state.values.schools.length !== action.schoolCount
+      ) {
         return state
-    }
-  }
-})
+      }
 
-export default form
+      return {
+        ...state,
+        values: {
+          ...state.values,
+          schools: state.values.schools.slice(0, -1)
+        }
+      }
+
+    case CLEAR_JOB_FIELD:
+      if (
+        !state.values ||
+        !state.values.jobs ||
+        state.values.jobs.length <= 1 ||
+        state.values.jobs.length !== action.jobCount
+      ) {
+        return state
+      }
+
+      return {
+        ...state,
+        values: {
+          ...state.values,
+          jobs: state.values.jobs.slice(0, -1)
+        }
+      }
+
+    case CLEAR_JOB_DUTY_FIELD:
+      if (
+        !state.values ||
+        !state.values.jobs ||
+        !state.values.jobs[action.index] ||
+        !state.values.jobs[action.index].duties ||
+        state.values.jobs[action.index].duties.length <= 1 ||
+        action.jobDutyCount !== state.values.jobs[action.index].duties.length
+      ) {
+        return state
+      }
+
+      return {
+        ...state,
+        values: {
+          ...state.values,
+          jobs: [
+            ...state.values.jobs.slice(0, action.index),
+            {
+              ...state.values.jobs[action.index],
+              duties: state.values.jobs[action.index].duties.slice(0, -1)
+            },
+            ...state.values.jobs.slice(action.index + 1)
+          ]
+        }
+      }
+
+    case CLEAR_PROJECT_FIELD:
+      if (
+        !state.values ||
+        !state.values.projects ||
+        state.values.projects.length <= 1 ||
+        state.values.projects.length !== action.projectCount
+      ) {
+        return state
+      }
+
+      return {
+        ...state,
+        values: {
+          ...state.values,
+          projects: state.values.projects.slice(0, -1)
+        }
+      }
+
+    case CLEAR_SKILL_FIELD:
+      if (
+        !state.values ||
+        !state.values.skills ||
+        state.values.skills.length <= 1 ||
+        state.values.skills.length !== action.skillCount
+      ) {
+        return state
+      }
+
+      return {
+        ...state,
+        values: {
+          ...state.values,
+          skills: state.values.skills.slice(0, -1)
+        }
+      }
+
+    default:
+      return state
+  }
+}
+
+export { form }
+export default reducer.plugin({ resume: form })

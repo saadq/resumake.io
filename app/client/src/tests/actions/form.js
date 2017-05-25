018fa59b5293ace7fbb11023247872110617b0c5
Saad Quadri <saad@saadquadri.com>
2017-05-25 04:59:18 -0400

    add tests for form actions

---
+++ app/client/src/tests/actions/form.js
@@ -0,0 +1,25 @@
+import test from 'ava'
+import { FormActions } from '../../actions'
+import {
+  CLEAR_SCHOOL_FIELD,
+  CLEAR_JOB_FIELD,
+  CLEAR_JOB_DUTY_FIELD,
+  CLEAR_PROJECT_FIELD,
+  CLEAR_SKILL_FIELD
+} from '../../constants'
+
+const {
+  clearSchoolField,
+  clearJobField,
+  clearJobDutyField,
+  clearProjectField,
+  clearSkillField
+} = FormActions
+
+test('form actions', async t => {
+  t.deepEqual(clearSchoolField(2), { type: CLEAR_SCHOOL_FIELD, schoolCount: 2 })
+  t.deepEqual(clearJobField(1), { type: CLEAR_JOB_FIELD, jobCount: 1 })
+  t.deepEqual(clearJobDutyField(0, 1), { type: CLEAR_JOB_DUTY_FIELD, index: 0, jobDutyCount: 1 })
+  t.deepEqual(clearProjectField(3), { type: CLEAR_PROJECT_FIELD, projectCount: 3 })
+  t.deepEqual(clearSkillField(4), { type: CLEAR_SKILL_FIELD, skillCount: 4 })
+})

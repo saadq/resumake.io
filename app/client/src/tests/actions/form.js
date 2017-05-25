f2f9740b2c97417147cca804e97b84379070f17c
Saad Quadri <saad@saadquadri.com>
2017-05-25 05:03:29 -0400

    fix code format

--- app/client/src/tests/actions/form.js
+++ app/client/src/tests/actions/form.js
@@ -19,7 +19,14 @@ const {
 test('form actions', async t => {
   t.deepEqual(clearSchoolField(2), { type: CLEAR_SCHOOL_FIELD, schoolCount: 2 })
   t.deepEqual(clearJobField(1), { type: CLEAR_JOB_FIELD, jobCount: 1 })
-  t.deepEqual(clearJobDutyField(0, 1), { type: CLEAR_JOB_DUTY_FIELD, index: 0, jobDutyCount: 1 })
-  t.deepEqual(clearProjectField(3), { type: CLEAR_PROJECT_FIELD, projectCount: 3 })
+  t.deepEqual(clearJobDutyField(0, 1), {
+    type: CLEAR_JOB_DUTY_FIELD,
+    index: 0,
+    jobDutyCount: 1
+  })
+  t.deepEqual(clearProjectField(3), {
+    type: CLEAR_PROJECT_FIELD,
+    projectCount: 3
+  })
   t.deepEqual(clearSkillField(4), { type: CLEAR_SKILL_FIELD, skillCount: 4 })
 })

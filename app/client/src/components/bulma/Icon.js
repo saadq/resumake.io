9b5f33de6604060d728e1f0648211563b7b960cc
Saad Quadri <saad@saadquadri.com>
2017-05-31 14:16:12 -0400

    add github icon to topbar

--- app/client/src/components/bulma/Icon.js
+++ app/client/src/components/bulma/Icon.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { string, node } from 'prop-types'
 
-function Icon({ size, type, children, ...props }) {
-  return (
+function Icon({ size, type, children, to, ...props }) {
+  const icon = (
     <span className={`icon ${size ? `is-${size}` : ''}`.trim()}>
       <i className={`fa fa-${type}`.trim()} {...props}>
         {children}
       </i>
     </span>
   )
+
+  return to ? <a className="nav-item" href={to}>{icon}</a> : icon
 }
 
 Icon.propTypes = {
--- app/client/src/components/layout/TopBar.js
+++ app/client/src/components/layout/TopBar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { bool, func } from 'prop-types'
 import { Link } from 'react-router-dom'
+import { Icon } from '../bulma'
 import '../../styles/components/top-bar.styl'
 
 function TopBar({ sideNavActive, toggleSideNav }) {
   return (
     <nav id="top-bar" className="nav">
-      <div className="nav-left" />
+      <div className="nav-left">
+        <Icon type="github" to="https://github.com/saadq/latexresu.me" />
+      </div>
       <div className="nav-center">
         <Link to="/" className="nav-item hero-brand">
           <span>LaTeX </span>

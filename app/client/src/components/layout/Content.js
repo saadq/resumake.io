df4f5b6daf672eb1469e7939a49c30368cb58306
Saad Quadri <saad@saadquadri.com>
2017-06-05 20:10:51 -0400

    start working with history

--- app/client/src/components/layout/Content.js
+++ app/client/src/components/layout/Content.js
@@ -1,28 +1,54 @@
-import React from 'react'
-import { func, node } from 'prop-types'
+import React, { Component } from 'react'
+import { func, node, object } from 'prop-types'
+import { withRouter } from 'react-router-dom'
 import '../../styles/components/content.styl'
 
-function Content({ hideSideNav, children }) {
-  return (
-    <div onClick={hideSideNav} id="content">
-      <button className="switch-section-button prev-section-button">
-        <span className="icon is-small">
-          <i className="fa fa-chevron-left" />
-        </span>
-      </button>
-      {children}
-      <button className="switch-section-button next-section-button">
-        <span className="icon is-small">
-          <i className="fa fa-chevron-right" />
-        </span>
-      </button>
-    </div>
-  )
+class Content extends Component {
+  componentDidMount() {
+    const { history } = this.props
+
+    this.unlisten = history.listen(({ pathname }) => {
+      if (pathname.startsWith('/generator/')) {
+        const section = pathname.slice(11)
+
+        console.log(history)
+
+        this.updateSectionNavigation(section)
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.unlisten()
+  }
+
+  updateSectionNavigation(section) {
+    console.log(section)
+  }
+
+  render() {
+    return (
+      <div onClick={this.props.hideSideNav} id="content">
+        <button className="switch-section-button prev-section-button">
+          <span className="icon is-small">
+            <i className="fa fa-chevron-left" />
+          </span>
+        </button>
+        {this.props.children}
+        <button className="switch-section-button next-section-button">
+          <span className="icon is-small">
+            <i className="fa fa-chevron-right" />
+          </span>
+        </button>
+      </div>
+    )
+  }
 }
 
 Content.propTypes = {
   hideSideNav: func.isRequired,
-  children: node.isRequired
+  children: node.isRequired,
+  history: object.isRequired
 }
 
-export default Content
+export default withRouter(Content)

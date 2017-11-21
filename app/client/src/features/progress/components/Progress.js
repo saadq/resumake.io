dc95e5bcce893c6e68c423444e75f50968f59d17
Saad Quadri <saad@saadq.com>
2017-11-21 11:23:59 -0500

    temp styling for progress bar/buttons

--- app/client/src/features/progress/components/Progress.js
+++ app/client/src/features/progress/components/Progress.js
@@ -26,21 +26,48 @@ const Buttons = styled.div`
 
 const Button = styled(Link)`
   color: white;
+  opacity: 0.5;
   text-decoration: none;
   transition: opacity 0.4s ease;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 0.75em;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  i {
+    font-size: 0.75em;
+    margin: 0 5px;
+  }
 `
 
 const PrevButton = Button.extend`
-  opacity: ${props => (props.section === 'templates' ? 0 : 1)};
+  opacity: ${props => (props.section === 'templates' ? 0 : 0.65)};
 `
 const NextButton = Button.extend`
-  opacity: ${props => (props.section === 'preview' ? 0 : 1)};
+  opacity: ${props => (props.section === 'preview' ? 0 : 0.65)};
 `
 
 const Bar = styled.div`
+  position: relative;
   height: 5px;
-  background: white;
+  background: #222;
   border-radius: 10px;
+
+  :before {
+    transition: width .75s ease;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    height: 100%;
+    width: 85%;
+    background: white;
+    border-radius: 10px;
+  }
 `
 
 const Row = styled.div`
@@ -56,12 +83,12 @@ function Progress() {
       <Buttons>
         <PrevButton section={section} to="/">
           <Row>
-            <Icon type="navigate_before" /> Prev
+            <Icon type="arrow_back" /> Prev
           </Row>
         </PrevButton>
         <NextButton section={section} to="/">
           <Row>
-            Next <Icon type="navigate_next" />
+            Next <Icon type="arrow_forward" />
           </Row>
         </NextButton>
       </Buttons>

159e69be7fe46f251750f29f8431bc031a79e38d
saadq <saad@saadq.com>
2017-12-31 11:13:04 -0500

    finish section ordering for template9

--- app/server/src/generator/templates/template9/index.js
+++ app/server/src/generator/templates/template9/index.js
@@ -4,325 +4,342 @@
 
 import { stripIndent, source } from 'common-tags'
 import { WHITESPACE } from '../constants'
-import type { SanitizedValues } from '../../../types'
+import type { SanitizedValues, Generator } from '../../../types'
 
-function template9({
-  basics,
-  education,
-  work,
-  projects,
-  skills,
-  awards
-}: SanitizedValues) {
-  return stripIndent`
-    \\documentclass[fontsize=11pt]{article}
-    ${generateHeader()}
-    \\begin{document}
-    ${generateProfileSection(basics)}
-    ${generateEducationSection(education)}
-    ${generateExperienceSection(work)}
-    ${generateSkillsSection(skills)}
-    ${generateProjectsSection(projects)}
-    ${generateAwardsSection(awards)}
-    ${WHITESPACE}
-    \\end{document}
-  `
+type Template9Generator = Generator & {
+  resumeHeader: () => string
 }
 
-function generateProfileSection(basics) {
-  if (!basics) {
-    return ''
-  }
-
-  const { name, email, phone, location = {}, website } = basics
-  const info = [email, phone, location.address, website]
-    .filter(Boolean)
-    .join(' | ')
-
-  return stripIndent`
-    \\MyName{${name || ''}}
-    \\bigskip
-    {\\small \\hfill ${info || ''}}
-  `
-}
-
-function generateEducationSection(education) {
-  if (!education || !education.schools) {
-    return ''
-  }
-
-  const lastSchoolIndex = education.schools.length - 1
-
-  return source`
-  %%% Education
-  %%% ------------------------------------------------------------
-  \\NewPart{${education.heading || 'Education'}}{}
-  ${education.schools.map((school, i) => {
-    const {
-      institution = '',
-      studyType,
-      area,
-      gpa = '',
-      location = '',
-      startDate = '',
-      endDate = ''
-    } = school
-
-    let degreeLine = ''
-    let nameLine = ''
-
-    if (studyType && area) {
-      degreeLine = `${studyType} ${area}`
-    } else if (studyType || area) {
-      degreeLine = studyType || area
+const generator: Template9Generator = {
+  profileSection(basics) {
+    if (!basics) {
+      return ''
     }
 
-    let dateRange = ''
-
-    if (startDate && endDate) {
-      dateRange = `${startDate} - ${endDate}`
-    } else if (startDate) {
-      dateRange = `${startDate} - Present`
-    } else {
-      dateRange = endDate
-    }
-
-    if (institution && location) {
-      nameLine += `${institution}, ${location}`
-    } else if (institution || location) {
-      nameLine = institution || location
-    }
-
-    if (gpa) {
-      nameLine += ` ${gpa}`
-    }
+    const { name, email, phone, location = {}, website } = basics
+    const info = [email, phone, location.address, website]
+      .filter(Boolean)
+      .join(' | ')
 
     return stripIndent`
-    \\EducationEntry
-        {${degreeLine}}
-        {${dateRange || ''}}
-        {${nameLine}${i < lastSchoolIndex ? '\\\\' : ''}}
+      \\MyName{${name || ''}}
+      \\bigskip
+      {\\small \\hfill ${info || ''}}
     `
-  })}
-  `
-}
+  },
 
-function generateExperienceSection(work) {
-  if (!work || !work.jobs) {
-    return ''
-  }
+  educationSection(education) {
+    if (!education || !education.schools) {
+      return ''
+    }
 
-  const lastJobIndex = work.jobs.length - 1
+    const lastSchoolIndex = education.schools.length - 1
 
-  return source`
-    %%% Work experience
-    %%% ------------------------------------------------------------
+    return source`
+      %%% Education
+      %%% ------------------------------------------------------------
+      \\NewPart{${education.heading || 'Education'}}{}
+      ${education.schools.map((school, i) => {
+        const {
+          institution = '',
+          studyType,
+          area,
+          gpa = '',
+          location = '',
+          startDate = '',
+          endDate = ''
+        } = school
 
-    \\NewPart{${work.heading || 'Experience'}}{}
+        let degreeLine = ''
+        let nameLine = ''
 
-    ${work.jobs.map((job, i) => {
-      const {
-        company,
-        position,
-        location,
-        startDate,
-        endDate,
-        highlights
-      } = job
+        if (studyType && area) {
+          degreeLine = `${studyType} ${area}`
+        } else if (studyType || area) {
+          degreeLine = studyType || area
+        }
 
-      const nameLine = [company, location].filter(Boolean).join(', ')
-      let dateRange = ''
-      let dutyLines = ''
+        let dateRange = ''
 
-      if (startDate && endDate) {
-        dateRange = `${startDate} - ${endDate}`
-      } else if (startDate) {
-        dateRange = `${startDate} - Present`
-      } else {
-        dateRange = endDate
-      }
+        if (startDate && endDate) {
+          dateRange = `${startDate} - ${endDate}`
+        } else if (startDate) {
+          dateRange = `${startDate} - Present`
+        } else {
+          dateRange = endDate
+        }
 
-      if (highlights) {
-        dutyLines = source`
+        if (institution && location) {
+          nameLine += `${institution}, ${location}`
+        } else if (institution || location) {
+          nameLine = institution || location
+        }
+
+        if (gpa) {
+          nameLine += ` ${gpa}`
+        }
+
+        return stripIndent`
+          \\EducationEntry
+            {${degreeLine}}
+            {${dateRange || ''}}
+            {${nameLine}${i < lastSchoolIndex ? '\\\\' : ''}}
+        `
+      })}
+    `
+  },
+
+  workSection(work) {
+    if (!work || !work.jobs) {
+      return ''
+    }
+
+    const lastJobIndex = work.jobs.length - 1
+
+    return source`
+      %%% Work experience
+      %%% ------------------------------------------------------------
+
+      \\NewPart{${work.heading || 'Experience'}}{}
+
+      ${work.jobs.map((job, i) => {
+        const {
+          company,
+          position,
+          location,
+          startDate,
+          endDate,
+          highlights
+        } = job
+
+        const nameLine = [company, location].filter(Boolean).join(', ')
+        let dateRange = ''
+        let dutyLines = ''
+
+        if (startDate && endDate) {
+          dateRange = `${startDate} - ${endDate}`
+        } else if (startDate) {
+          dateRange = `${startDate} - Present`
+        } else {
+          dateRange = endDate
+        }
+
+        if (highlights) {
+          dutyLines = source`
             \\begin{itemize} \\itemsep -1pt
               ${highlights.map(duty => `\\item ${duty}`)}
             \\end{itemize}
           `
-      }
+        }
 
-      return stripIndent`
+        return stripIndent`
           \\WorkEntry
             {${position || ''}}
             {${dateRange || ''}}
             {${nameLine}}
             {${dutyLines}}
             ${i < lastJobIndex ? '\\sepspace' : ''}
-      `
-    })}
-  `
-}
+        `
+      })}
+    `
+  },
 
-function generateSkillsSection(skills) {
-  if (!skills || !skills.skills) {
-    return ''
-  }
+  skillsSection(skills) {
+    if (!skills || !skills.skills) {
+      return ''
+    }
 
-  return source`
-  %%% Skills
-  %%% ------------------------------------------------------------
-  \\NewPart{${skills.heading || 'Skills'}}{}
-  ${skills.skills.map(skill => {
-    const { name, keywords = [] } = skill
-    return `\\SkillsEntry{${name || ''}}{${keywords.join(', ')}}`
-  })}
-  `
-}
+    return source`
+      %%% Skills
+      %%% ------------------------------------------------------------
+      \\NewPart{${skills.heading || 'Skills'}}{}
+      ${skills.skills.map(skill => {
+        const { name, keywords = [] } = skill
+        return `\\SkillsEntry{${name || ''}}{${keywords.join(', ')}}`
+      })}
+    `
+  },
 
-function generateProjectsSection(projects) {
-  if (!projects || !projects.projects) {
-    return ''
-  }
+  projectsSection(projects) {
+    if (!projects || !projects.projects) {
+      return ''
+    }
 
-  const lastProjectIndex = projects.projects.length - 1
+    const lastProjectIndex = projects.projects.length - 1
 
-  return source`
-    %%% Projects
-    %%% ------------------------------------------------------------
-    \\NewPart{${projects.heading || 'Projects'}}{}
+    return source`
+      %%% Projects
+      %%% ------------------------------------------------------------
+      \\NewPart{${projects.heading || 'Projects'}}{}
 
-    ${projects.projects.map((project, i) => {
-      const { name, description, keywords = [], url } = project
+      ${projects.projects.map((project, i) => {
+        const { name, description, keywords = [], url } = project
 
-      return stripIndent`
+        return stripIndent`
           \\ProjectEntry{${name || ''}}{${url || ''}}
           {${keywords.join(', ')}}
           {${description || ''}}
           ${i < lastProjectIndex ? '\\sepspace' : ''}
-      `
-    })}
+        `
+      })}
+    `
+  },
 
-  `
-}
+  awardsSection(awards) {
+    if (!awards || !awards.awards) {
+      return ''
+    }
 
-function generateAwardsSection(awards) {
-  if (!awards || !awards.awards) {
-    return ''
-  }
+    const lastAwardIndex = awards.awards.length - 1
 
-  const lastAwardIndex = awards.awards.length - 1
+    return source`
+      %%% Awards
+      %%% ------------------------------------------------------------
+      \\NewPart{${awards.heading || 'Awards'}}{}
 
-  return source`
-    %%% Awards
-    %%% ------------------------------------------------------------
-    \\NewPart{${awards.heading || 'Awards'}}{}
+      ${awards.awards.map((award, i) => {
+        const { title, summary, date, awarder } = award
 
-    ${awards.awards.map((award, i) => {
-      const { title, summary, date, awarder } = award
-
-      return stripIndent`
+        return stripIndent`
           \\AwardEntry{${title || ''}}{${awarder || ''}}
           {${date || ''}}
           {${summary || ''}}
           ${i < lastAwardIndex ? '\\sepspace' : ''}
-      `
-    })}
+        `
+      })}
+    `
+  },
 
-  `
+  resumeHeader() {
+    return stripIndent`
+      \\usepackage[english]{babel}
+      \\usepackage[utf8x]{inputenc}
+      \\usepackage[protrusion=true,expansion=true]{microtype}
+      \\usepackage[svgnames]{xcolor}  % Colours by their 'svgnames'
+      \\usepackage[margin=0.75in]{geometry}
+        \\textheight=700px
+      \\usepackage{url}
+      \\usepackage{lmodern} % Allow arbitrary font sizes
+      \\usepackage{textcomp}
+
+      %% Define a new 'modern' style for the url package that will use a smaller font.
+      \\makeatletter
+      \\def\\url@modernstyle{
+        \\@ifundefined{selectfont}{\\def\\UrlFont{\\sf}}{\\def\\UrlFont{}}}
+      \\makeatother
+      \\urlstyle{modern} %% And use the newly defined style.
+
+      \\frenchspacing              % Better looking spacings after periods
+      \\pagestyle{empty}           % No pagenumbers/headers/footers
+
+      \\renewcommand{\\familydefault}{\\sfdefault}
+
+      %%% Custom sectioning (sectsty package)
+      %%% ------------------------------------------------------------
+      \\usepackage{sectsty}
+
+      \\sectionfont{                 % Change font of \\section command
+        \\usefont{OT1}{phv}{b}{n}%   % bch-b-n: CharterBT-Bold font
+        \\sectionrule{0pt}{0pt}{-5pt}{3pt}}
+
+      %%% Macros
+      %%% ------------------------------------------------------------
+      \\newlength{\\spacebox}
+      \\settowidth{\\spacebox}{8888888888}      % Box to align text
+      \\newcommand{\\sepspace}{\\vspace*{1em}}   % Vertical space macro
+
+      \\newcommand{\\MyName}[1]{ % Name
+          \\Huge \\usefont{OT1}{phv}{b}{n} \\hfill #1
+          \\par \\normalsize \\normalfont}
+
+      \\newcommand{\\MySlogan}[1]{ % Slogan (optional)
+          \\large \\usefont{OT1}{phv}{m}{n}\\hfill \\textit{#1}
+          \\par \\normalsize \\normalfont}
+
+      \\newcommand{\\NewPart}[1]{\\section*{\\uppercase{#1}}}
+
+      \\newcommand{\\PersonalEntry}[2]{
+          \\noindent\\hangindent=2em\\hangafter=0 % Indentation
+          \\parbox{\\spacebox}{                  % Box to align text
+          \\textit{#1}}                      % Entry name (birth, address, etc.)
+          \\hspace{1.5em} #2 \\par}              % Entry value
+
+      \\newcommand{\\SkillsEntry}[2]{                % Same as \\PersonalEntry
+          \\noindent\\hangindent=2em\\hangafter=0 % Indentation
+          \\parbox{\\spacebox}{                  % Box to align text
+          \\textit{#1}}                    % Entry name (birth, address, etc.)
+          \\hspace{1.5em} #2 \\par}              % Entry value
+
+      \\newcommand{\\AwardsEntry}[2]{                % Same as \\PersonalEntry
+          \\noindent\\hangindent=2em\\hangafter=0 % Indentation
+          \\parbox{\\spacebox}{                  % Box to align text
+          \\textit{#1}}                    % Entry name (birth, address, etc.)
+          \\hspace{1.5em} #2 \\par}              % Entry value
+
+      \\newcommand{\\EducationEntry}[4]{
+          \\noindent \\textbf{#1} \\hfill      % Study
+          \\colorbox{Black}{
+            \\parbox{8.5em}{
+            \\hfill\\color{White}#2}} \\par  % Duration
+          \\noindent \\textit{#3} \\par        % School
+          \\noindent\\hangindent=2em\\hangafter=0 \\small #4 % Description
+          \\normalsize \\par}
+
+      \\newcommand{\\WorkEntry}[4]{       % Same as \\EducationEntry
+          \\noindent \\textbf{#1} \\hfill      % Jobname
+          \\colorbox{Black}{%
+            \\parbox{9em}{%
+            \\hfill\\color{White}#2}} \\par   % Duration
+              \\noindent \\textit{#3} \\par        % Company
+          \\noindent\\hangindent=2em\\hangafter=0 \\small #4 % Description
+          \\normalsize \\par}
+
+      \\newcommand{\\ProjectEntry}[4]{         % Similar to \\EducationEntry
+          \\noindent \\textbf{#1} \\noindent \\textit{#3} \\hfill {#2} \\par
+          \\noindent \\small #4 % Description
+          \\normalsize \\par}
+
+      \\newcommand{\\AwardEntry}[4]{         % Similar to \\EducationEntry
+          \\noindent \\textbf{#1} \\noindent \\textit{#3} \\hfill {#2} \\par
+          \\noindent \\small #4 % Description
+          \\normalsize \\par}
+    `
+  }
 }
 
-function generateHeader() {
+function template9(values: SanitizedValues) {
   return stripIndent`
-    \\usepackage[english]{babel}
-    \\usepackage[utf8x]{inputenc}
-    \\usepackage[protrusion=true,expansion=true]{microtype}
-    \\usepackage[svgnames]{xcolor}  % Colours by their 'svgnames'
-    \\usepackage[margin=0.75in]{geometry}
-      \\textheight=700px
-    \\usepackage{url}
-    \\usepackage{lmodern} % Allow arbitrary font sizes
-    \\usepackage{textcomp}
+    \\documentclass[fontsize=11pt]{article}
+    ${generator.resumeHeader()}
+    \\begin{document}
+    ${values.orderedSections
+      .map(section => {
+        switch (section) {
+          case 'profile':
+            return generator.profileSection(values.basics)
 
-    %% Define a new 'modern' style for the url package that will use a smaller font.
-    \\makeatletter
-    \\def\\url@modernstyle{
-      \\@ifundefined{selectfont}{\\def\\UrlFont{\\sf}}{\\def\\UrlFont{}}}
-    \\makeatother
-    \\urlstyle{modern} %% And use the newly defined style.
+          case 'education':
+            return generator.educationSection(values.education)
 
-    \\frenchspacing              % Better looking spacings after periods
-    \\pagestyle{empty}           % No pagenumbers/headers/footers
+          case 'work':
+            return generator.workSection(values.work)
 
-    \\renewcommand{\\familydefault}{\\sfdefault}
+          case 'skills':
+            return generator.skillsSection(values.skills)
 
-    %%% Custom sectioning (sectsty package)
-    %%% ------------------------------------------------------------
-    \\usepackage{sectsty}
+          case 'projects':
+            return generator.projectsSection(values.projects)
 
-    \\sectionfont{                 % Change font of \\section command
-      \\usefont{OT1}{phv}{b}{n}%   % bch-b-n: CharterBT-Bold font
-      \\sectionrule{0pt}{0pt}{-5pt}{3pt}}
+          case 'awards':
+            return generator.awardsSection(values.awards)
 
-    %%% Macros
-    %%% ------------------------------------------------------------
-    \\newlength{\\spacebox}
-    \\settowidth{\\spacebox}{8888888888}      % Box to align text
-    \\newcommand{\\sepspace}{\\vspace*{1em}}   % Vertical space macro
-
-    \\newcommand{\\MyName}[1]{ % Name
-        \\Huge \\usefont{OT1}{phv}{b}{n} \\hfill #1
-        \\par \\normalsize \\normalfont}
-
-    \\newcommand{\\MySlogan}[1]{ % Slogan (optional)
-        \\large \\usefont{OT1}{phv}{m}{n}\\hfill \\textit{#1}
-        \\par \\normalsize \\normalfont}
-
-    \\newcommand{\\NewPart}[1]{\\section*{\\uppercase{#1}}}
-
-    \\newcommand{\\PersonalEntry}[2]{
-        \\noindent\\hangindent=2em\\hangafter=0 % Indentation
-        \\parbox{\\spacebox}{                  % Box to align text
-        \\textit{#1}}                      % Entry name (birth, address, etc.)
-        \\hspace{1.5em} #2 \\par}              % Entry value
-
-    \\newcommand{\\SkillsEntry}[2]{                % Same as \\PersonalEntry
-        \\noindent\\hangindent=2em\\hangafter=0 % Indentation
-        \\parbox{\\spacebox}{                  % Box to align text
-        \\textit{#1}}                    % Entry name (birth, address, etc.)
-        \\hspace{1.5em} #2 \\par}              % Entry value
-
-    \\newcommand{\\AwardsEntry}[2]{                % Same as \\PersonalEntry
-        \\noindent\\hangindent=2em\\hangafter=0 % Indentation
-        \\parbox{\\spacebox}{                  % Box to align text
-        \\textit{#1}}                    % Entry name (birth, address, etc.)
-        \\hspace{1.5em} #2 \\par}              % Entry value
-
-    \\newcommand{\\EducationEntry}[4]{
-        \\noindent \\textbf{#1} \\hfill      % Study
-        \\colorbox{Black}{
-          \\parbox{8.5em}{
-          \\hfill\\color{White}#2}} \\par  % Duration
-        \\noindent \\textit{#3} \\par        % School
-        \\noindent\\hangindent=2em\\hangafter=0 \\small #4 % Description
-        \\normalsize \\par}
-
-    \\newcommand{\\WorkEntry}[4]{       % Same as \\EducationEntry
-        \\noindent \\textbf{#1} \\hfill      % Jobname
-        \\colorbox{Black}{%
-          \\parbox{9em}{%
-          \\hfill\\color{White}#2}} \\par   % Duration
-            \\noindent \\textit{#3} \\par        % Company
-        \\noindent\\hangindent=2em\\hangafter=0 \\small #4 % Description
-        \\normalsize \\par}
-
-    \\newcommand{\\ProjectEntry}[4]{         % Similar to \\EducationEntry
-        \\noindent \\textbf{#1} \\noindent \\textit{#3} \\hfill {#2} \\par
-        \\noindent \\small #4 % Description
-        \\normalsize \\par}
-
-    \\newcommand{\\AwardEntry}[4]{         % Similar to \\EducationEntry
-        \\noindent \\textbf{#1} \\noindent \\textit{#3} \\hfill {#2} \\par
-        \\noindent \\small #4 % Description
-        \\normalsize \\par}
+          default:
+            return ''
+        }
+      })
+      .join('\n')}
+    ${WHITESPACE}
+    \\end{document}
   `
 }
 

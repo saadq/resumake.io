9e49d39aabd2cfa6e6d8dc97261b1b0e39058749
saadq <saad@saadq.com>
2018-01-14 15:49:06 -0500

    add middleware tests

---
+++ app/server/tests/middleware.js
@@ -0,0 +1,96 @@
+/**
+ * @flow
+ */
+
+import { errorHandler, sanitizer, jsonResume } from '../src/middleware'
+import { join } from 'path'
+
+test('errorHandler should catch all errors downstream', async () => {
+  const ctx: Object = {
+    app: {
+      emit: jest.fn()
+    }
+  }
+
+  const next = jest.fn(() => {
+    const err: Object = new Error('Bad user request')
+    err.status = 400
+    throw err
+  })
+
+  const middleware = errorHandler()
+  await middleware(ctx, next)
+
+  expect(ctx.status).toBe(400)
+  expect(ctx.body).toBe('Bad user request')
+})
+
+test('sanitizer should normalize the request body', async () => {
+  const ctx: Object = {
+    request: {
+      body: {
+        selectedTemplate: 1,
+        basics: {
+          name: 'Saad Quadri',
+          email: null,
+          website: 'github.com/saadq'
+        },
+        education: [],
+        work: [{ company: 'Johnson & Johnson' }],
+        skills: []
+      }
+    }
+  }
+
+  const next = jest.fn()
+
+  const expected = {
+    selectedTemplate: 1,
+    basics: {
+      name: 'Saad Quadri',
+      website: 'github.com/saadq'
+    },
+    work: [{ company: 'Johnson \\& Johnson' }]
+  }
+
+  const middleware = sanitizer()
+  await middleware(ctx, next)
+
+  expect(next).toHaveBeenCalled()
+  expect(ctx.request.body).toEqual(expected)
+})
+
+test('jsonResume should validate and extract the JSON from file', async () => {
+  const ctx: Object = {
+    request: {
+      files: {
+        'json-file': {
+          path: join(__dirname, 'utils', 'upload.json')
+        }
+      }
+    },
+    throw: jest.fn()
+  }
+
+  const next = jest.fn()
+
+  const expected = {
+    selectedTemplate: 1,
+    basics: {
+      name: 'Saad Quadri',
+      email: 'saad@saadq.com',
+      website: 'github.com/saadq'
+    },
+    education: [],
+    work: [{ highlights: [] }],
+    skills: [{ keywords: [] }],
+    projects: [{ keywords: [] }],
+    awards: []
+  }
+
+  const middleware = jsonResume()
+  await middleware(ctx, next)
+
+  expect(next).toHaveBeenCalled()
+  expect(ctx.request.jsonResume).toEqual(expected)
+})

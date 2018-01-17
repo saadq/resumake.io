c1d2956bbced9970886ba9c65ecc0d988aae05de
saadq <saad@saadq.com>
2018-01-16 22:01:19 -0500

    remove koa-static (just use nginx to serve static files)

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -4,10 +4,8 @@
 
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
-import serve from 'koa-static'
 import router from './routes'
 import { errorHandler } from './middleware'
-import { join } from 'path'
 
 const app = new Koa()
 
@@ -16,7 +14,6 @@ if (app.env === 'development') {
 }
 
 app.use(errorHandler())
-app.use(serve(join(__dirname, '..', '..', 'client', 'dist')))
 app.use(bodyParser())
 app.use(router)
 

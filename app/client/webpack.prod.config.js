97418bf540f8dea9a009d0875e81d8d063b17c41
saadq <saad@saadq.com>
2018-01-16 21:51:51 -0500

    don't use webpack build for react-pdf

--- app/client/webpack.prod.config.js
+++ app/client/webpack.prod.config.js
@@ -47,7 +47,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify('production')
       }
     })
   ]

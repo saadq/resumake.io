d38ea1715016ff09c45fb076fbcd64b311bb3777
saadq <saad@saadq.com>
2018-01-26 09:16:24 -0500

    use entry.webpack for react-pdf

--- app/client/src/features/preview/components/Preview.js
+++ app/client/src/features/preview/components/Preview.js
@@ -4,7 +4,7 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Document, Page } from 'react-pdf/build/entry.noworker'
+import { Document, Page } from 'react-pdf/build/entry.webpack'
 import styled from 'styled-components'
 import { Toolbar, LoadingBar } from '.'
 import { downloadSource } from '../actions'

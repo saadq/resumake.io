4b02cd643b14cc1cd1ee136c3b941bf78fb8bd2f
saadq <saad@saadq.com>
2020-06-21 03:31:51 -0700

    Refactor form slice

--- app/client/src/generator/resume-form/slice.ts
+++ app/client/src/generator/resume-form/slice.ts
@@ -1,5 +1,5 @@
 import { reducer as reduxFormReducer } from 'redux-form'
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, CaseReducer, PayloadAction } from '@reduxjs/toolkit'
 import { FormState, EmptyFields } from './types/form'
 import { CustomSectionTypes } from './types/sections'
 import {
@@ -11,110 +11,112 @@ import {
 
 type ResumeFormState = FormState['resume']
 
+type ActionReducer<ActionPayload = void> = CaseReducer<
+  ResumeFormState,
+  PayloadAction<ActionPayload>
+>
+
 const initialState: ResumeFormState = {
   values: { ...defaultFormValues },
   customSectionIndex: 1,
   registeredFields: []
 }
 
+const addSubsection: ActionReducer<{
+  sectionName: string
+  emptyFields: EmptyFields
+}> = (state, action) => {
+  const { sectionName, emptyFields } = action.payload
+  state.values[sectionName].push(emptyFields)
+}
+
+const removeSubsection: ActionReducer<{
+  sectionName: string
+  indexToRemove: number
+}> = (state, action) => {
+  const { sectionName, indexToRemove } = action.payload
+  state.values[sectionName].splice(indexToRemove, 1)
+}
+
+const addSubsectionKeyword: ActionReducer<{
+  sectionName: string
+  sectionIndex: number
+  keywordsName: string
+}> = (state, action) => {
+  const { sectionName, sectionIndex, keywordsName } = action.payload
+  state.values[sectionName][sectionIndex][keywordsName].push('')
+}
+
+const removeSubsectionKeyword: ActionReducer<{
+  sectionName: string
+  sectionIndex: number
+  keywordsName: string
+  keywordIndexToRemove: number
+}> = (state, action) => {
+  const {
+    sectionName,
+    sectionIndex,
+    keywordsName,
+    keywordIndexToRemove
+  } = action.payload
+  state.values[sectionName][sectionIndex][keywordsName].splice(
+    keywordIndexToRemove,
+    1
+  )
+}
+
+const swapSubsectionsOrder: ActionReducer<{
+  sectionName: string
+  startIndex: number
+  endIndex: number
+}> = (state, action) => {
+  const { sectionName, startIndex, endIndex } = action.payload
+  const [removed] = state.values[sectionName].splice(startIndex, 1)
+  state.values[sectionName].splice(endIndex, 0, removed)
+}
+
+const addCustomSection: ActionReducer = (state, action) => {
+  const sectionName = `custom-${state.customSectionIndex}`
+  state.values[sectionName] = [emptyTableSubsection]
+  state.values.sections.push({
+    type: 'table',
+    name: sectionName,
+    displayName: `Unnamed ${state.customSectionIndex}`
+  })
+  state.customSectionIndex += 1
+}
+
+const setCustomSectionType: ActionReducer<{
+  sectionName: string
+  sectionType: CustomSectionTypes
+}> = (state, action) => {
+  const { sectionName, sectionType } = action.payload
+  const oldSection = state.values.sections.find(
+    (section) => section.name === sectionName
+  )
+  if (!oldSection) return
+
+  oldSection.type = sectionType
+  if (sectionType === 'bullets') {
+    state.values[sectionName] = [emptyBulletsSubsection]
+  } else if (sectionType === 'table') {
+    state.values[sectionName] = [emptyTableSubsection]
+  } else if (sectionType === 'paragraph') {
+    state.values[sectionName] = [emptyParagraphSubsection]
+  }
+}
+
 const formSlice = createSlice({
   name: 'resume',
   initialState,
   reducers: {
-    addSubsection(
-      state: ResumeFormState,
-      action: PayloadAction<{ sectionName: string; emptyFields: EmptyFields }>
-    ) {
-      const { sectionName, emptyFields } = action.payload
-      state.values[sectionName].push(emptyFields)
-    },
-
-    removeSubsection(
-      state: ResumeFormState,
-      action: PayloadAction<{ sectionName: string; indexToRemove: number }>
-    ) {
-      const { sectionName, indexToRemove } = action.payload
-      state.values[sectionName].splice(indexToRemove, 1)
-    },
-
-    addSubsectionKeyword(
-      state: ResumeFormState,
-      action: PayloadAction<{
-        sectionName: string
-        sectionIndex: number
-        keywordsName: string
-      }>
-    ) {
-      const { sectionName, sectionIndex, keywordsName } = action.payload
-      state.values[sectionName][sectionIndex][keywordsName].push('')
-    },
-
-    removeSubsectionKeyword(
-      state: ResumeFormState,
-      action: PayloadAction<{
-        sectionName: string
-        sectionIndex: number
-        keywordsName: string
-        keywordIndexToRemove: number
-      }>
-    ) {
-      const {
-        sectionName,
-        sectionIndex,
-        keywordsName,
-        keywordIndexToRemove
-      } = action.payload
-      state.values[sectionName][sectionIndex][keywordsName].splice(
-        keywordIndexToRemove,
-        1
-      )
-    },
-
-    swapSubsectionsOrder(
-      state: ResumeFormState,
-      action: PayloadAction<{
-        sectionName: string
-        startIndex: number
-        endIndex: number
-      }>
-    ) {
-      const { sectionName, startIndex, endIndex } = action.payload
-      const [removed] = state.values[sectionName].splice(startIndex, 1)
-      state.values[sectionName].splice(endIndex, 0, removed)
-    },
-
-    addCustomSection(state: ResumeFormState, action: PayloadAction) {
-      const sectionName = `custom-${state.customSectionIndex}`
-      state.values[sectionName] = [emptyTableSubsection]
-      state.values.sections.push({
-        type: 'table',
-        name: sectionName,
-        displayName: `Unnamed ${state.customSectionIndex}`
-      })
-      state.customSectionIndex += 1
-    },
-
-    setCustomSectionType(
-      state: ResumeFormState,
-      action: PayloadAction<{
-        sectionName: string
-        sectionType: CustomSectionTypes
-      }>
-    ) {
-      const { sectionName, sectionType } = action.payload
-      const oldSection = state.values.sections.find(
-        (section) => section.name === sectionName
-      )
-      oldSection!.type = sectionType
-
-      if (sectionType === 'bullets') {
-        state.values[sectionName] = [emptyBulletsSubsection]
-      } else if (sectionType === 'table') {
-        state.values[sectionName] = [emptyTableSubsection]
-      } else if (sectionType === 'paragraph') {
-        state.values[sectionName] = [emptyParagraphSubsection]
-      }
-    }
+    addSubsection,
+    removeSubsection,
+    addSubsectionKeyword,
+    removeSubsectionKeyword,
+    swapSubsectionsOrder,
+    addCustomSection,
+    setCustomSectionType
   }
 })
 

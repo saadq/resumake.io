4564ebc3499d62cb1c480d8e1f06f1a12013eccb
saadq <saad@saadq.com>
2020-06-22 05:19:08 -0700

    Move temp submit button to toolbar

--- app/client/src/generator/resume-preview/components/Toolbar.tsx
+++ app/client/src/generator/resume-preview/components/Toolbar.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Header } from 'common/components/Header'
+
+const ToolbarHeader = styled(Header)`
+  display: flex;
+  justify-content: center;
+`
 
 interface Props {
   prevPage: () => void
@@ -9,5 +15,9 @@ interface Props {
 }
 
 export function Toolbar({ prevPage, nextPage, zoomIn, zoomOut }: Props) {
-  return <Header accent />
+  return (
+    <ToolbarHeader accent>
+      <button form="resume-form">Submit</button>
+    </ToolbarHeader>
+  )
 }

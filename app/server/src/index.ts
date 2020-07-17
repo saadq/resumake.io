3e6952ea1653db08429beae5d10b1d4a39a5c9e6
Mohamed Daahir <sharaf.13@hotmail.com>
2020-07-17 02:58:33 +0100

    Add logging to the backend (#139)
    * add logging to koa
    
    * remove quotes around ts-node to support windows

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,11 +1,13 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
+import logger from 'koa-logger'
 import { api } from './routes/api'
 import { errorHandler } from './middleware/error-handler'
 
 const app = new Koa()
 const port = process.env.PORT || 4001
 
+app.use(logger())
 app.use(errorHandler())
 app.use(bodyParser())
 app.use(api.routes())

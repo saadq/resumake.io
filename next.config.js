e01303eab146a3eebf9585ae643769e92f238d25
ducaale <sharaf.13@hotmail.com>
2022-07-17 11:54:33 +0300

    re-enable type checking in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+// @ts-check
+
+/** @type {import('next').NextConfig} **/
+const NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true
   },
 }
+
+module.exports = NextConfig

b518736a9ad68595b598902df43b078622ee0e6e
ducaale <sharaf.13@hotmail.com>
2022-10-16 20:50:38 +0100

    port resume v2's home page
    This is not 100% exact match but we will get there :)

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,7 +1,54 @@
-export function Button() {
-  return (
-    <button>
-      <span>hi</span>
-    </button>
-  )
-}
+import styled from 'styled-components'
+import { lighten, darken, rgba } from 'polished'
+import { colors } from '../../theme'
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: inherit;
+  font-size: 0.85em;
+  text-align: center;
+  text-decoration: none;
+  width: 175px;
+  height: 45px;
+  margin: 7px 0;
+  background: transparent;
+  color: white;
+  border-radius: 100px;
+  border: 1px solid ${darken(0.1, colors.primary)};
+  box-shadow: 0 0 0 0 ${rgba(colors.primary, 0.7)};
+  transition: all 0.4s ease;
+  &:hover {
+    background: linear-gradient(
+      40deg,
+      ${darken(0.5, colors.primary)},
+      ${darken(0.3, colors.primary)}
+    );
+    animation: none;
+    cursor: pointer;
+  }
+  &:active {
+    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.06), 0 2px 40px rgba(0, 0, 0, 0.16);
+    border-color: ${lighten(0.15, colors.primary)};
+    color: ${lighten(0.15, colors.primary)};
+  }
+  &:focus {
+    outline: none;
+  }
+`
+
+export const PrimaryButton = styled(Button)`
+  background: linear-gradient(
+    40deg,
+    ${darken(0.3, colors.primary)},
+    ${colors.primary}
+  );
+  &:hover {
+    background: linear-gradient(
+      40deg,
+      ${darken(0.4, colors.primary)},
+      ${colors.primary}
+    );
+  }
+`
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,68 @@
 import Link from 'next/link'
+import styled from 'styled-components'
+import { lighten, darken } from 'polished'
+import { Logo } from '../common/Logo'
+import { PrimaryButton, Button } from '../common/Button'
+import { colors } from '../../theme'
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr 75px;
+`
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const Footer = styled.footer`
+  background: ${darken(0.02, colors.background)};
+  border-top: 1px solid ${colors.borders};
+  padding: 0 50px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2em;
+  font-size: 0.8em;
+  color: ${lighten(0.3, colors.background)};
+
+  @media screen and (max-width: 850px) {
+    padding: 0 15px;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${lighten(0.3, colors.background)};
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
+
+const Copyright = styled.span`
+  flex: 1;
+`
 
 export function Home() {
   return (
-    <>
-      <h1>Home</h1>
-
-      <Link href="/generator">Generator</Link>
-    </>
+    <Wrapper>
+      <Main>
+        <Logo marginBottom='0.75em' />
+        <Link href="/generator">
+          <PrimaryButton>Make New Resume</PrimaryButton>
+        </Link>
+        <Button>Import JSON</Button>
+      </Main>
+      <Footer>
+        <Copyright>© 2022 Saad Quadri</Copyright>
+        <Link href="/about">About</Link>
+        <a href="https://github.com/saadq/resumake">Source</a>
+        <a href="https://github.com/saadq/resumake/issues">Issues</a>
+      </Footer>
+    </Wrapper>
   )
 }

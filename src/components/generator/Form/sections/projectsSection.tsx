96067688d611de2dcd96030dbbb96dd32db9380c
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2024-01-24 10:02:38 -0800

    Some changes

--- src/components/generator/Form/sections/projectsSection.tsx
+++ src/components/generator/Form/sections/projectsSection.tsx
@@ -6,29 +6,47 @@ import { AddButton } from '../../../core/Button'
 import { Divider } from '../../../core/Divider'
 import { FormSection } from './FormSection'
 import Keywords from '../Keywords'
+import Highlights from '../Highlights'
+import { Project } from '../../../../types'
 
 export function ProjectsSection() {
   const { fields, append } = useFieldArray({ name: 'projects' })
 
+  const handleAdd = () => {
+    const defaultProject: Project = {
+      name: '',
+      description: '',
+      url: '',
+      keywords: [],
+      highlights: []
+    }
+
+    append(defaultProject)
+  }
+
   return (
     <FormSection title="Your Projects">
-      <LabeledInput
-        name="headings.projects"
-        label="Section Heading"
-        placeholder="Projects"
-      />
+      {fields.length > 0 && (
+        <Fragment>
+          <LabeledInput
+            name="headings.projects"
+            label="Section Heading"
+            placeholder="Projects"
+          />
+          <Divider />
+        </Fragment>
+      )}
       {fields.map((field, index) => (
         <Fragment key={field.id}>
-          <Divider />
           <LabeledInput
             name={`projects.${index}.name`}
             label="Project Name"
             placeholder="Piper Chat"
           />
-          <LabeledInput
-            name={`projects.${index}.description`}
-            label="Project Description"
-            placeholder="A video chat app with great picture quality."
+          <Highlights
+            label="Project Details"
+            placeholder="Won CalHacks 2023"
+            name={`projects.${index}.highlights`}
           />
           <LabeledInput
             name={`projects.${index}.url`}
@@ -40,9 +58,10 @@ export function ProjectsSection() {
             placeholder="TypeScript"
             name={`projects.${index}.keywords`}
           />
+          <Divider />
         </Fragment>
       ))}
-      <AddButton type="button" onClick={() => append({})}>
+      <AddButton type="button" onClick={handleAdd}>
         + Add Project
       </AddButton>
     </FormSection>

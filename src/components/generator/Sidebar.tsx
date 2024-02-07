4c2571cfc82fc1ed6a963b63f99ffaa4bde93448
Nishanth Mane <nishath.engg93@gmail.com>
2024-02-07 19:21:04 +0530

    feat: (section-list): Allow user to drag and drop to sort the sections in the sidebar

--- src/components/generator/Sidebar.tsx
+++ src/components/generator/Sidebar.tsx
@@ -2,9 +2,11 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
 import { MdDragIndicator } from 'react-icons/md'
+import DragSort from 'react-dragged'
 
 import { colors } from '../../theme'
 import { PrimaryButton, IconButton } from '../core/Button'
+import { useState } from 'react'
 
 const Aside = styled.aside`
   grid-area: sidebar;
@@ -12,7 +14,8 @@ const Aside = styled.aside`
   padding: 24px 36px;
 `
 
-const Nav = styled.nav`
+// div targets the items inside the drag sort library
+const NavList = styled.nav`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -20,8 +23,11 @@ const Nav = styled.nav`
   gap: 24px;
   margin-bottom: 28px;
 
-  button {
-    cursor: grab;
+  > div {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    gap: 24px;
   }
 `
 
@@ -33,38 +39,82 @@ const StyledLink = styled(Link)<{ $active: boolean }>`
   ${(props) => props.$active && `color: ${colors.primary};`}
 `
 
+const DragIconButton = styled(IconButton)`
+  opacity: ${(props) => (props.disabled ? 0 : 1)};
+  cursor: ${(props) => (props.disabled ? 'default' : 'grab')};
+`
+
+const staticSectionLinks = [
+  { label: 'Templates', section: 'templates' },
+  { label: 'Profile', section: 'basics' }
+]
+
+const sortableSectionLinks = [
+  { label: 'Education', section: 'education' },
+  { label: 'Work Experience', section: 'work' },
+  { label: 'Skills', section: 'skills' },
+  { label: 'Projects', section: 'projects' },
+  { label: 'Awards', section: 'awards' }
+]
+
+const NavItem = ({
+  label,
+  section,
+  currSection,
+  draggable
+}: {
+  label: string
+  section: string
+  currSection: string
+  draggable?: boolean
+}) => (
+  <div key={section} style={{ display: 'flex', gap: 8 }}>
+    <DragIconButton type="button" disabled={!Boolean(draggable)}>
+      <MdDragIndicator />
+    </DragIconButton>
+    <StyledLink
+      href={`/generator?section=${section}`}
+      $active={section === currSection}
+    >
+      {label}
+    </StyledLink>
+  </div>
+)
+
 export function Sidebar() {
   const router = useRouter()
-  const { section: currSection = 'basics' } = router.query
-
-  const sectionLinks = [
-    { label: 'Templates', section: 'templates' },
-    { label: 'Profile', section: 'basics' },
-    { label: 'Education', section: 'education' },
-    { label: 'Work Experience', section: 'work' },
-    { label: 'Skills', section: 'skills' },
-    { label: 'Projects', section: 'projects' },
-    { label: 'Awards', section: 'awards' }
-  ]
+  const currSection = (router.query.section || 'basics') as string
+  const [sections, updateSectionOrder] = useState(sortableSectionLinks)
 
   return (
     <Aside>
-      <Nav>
-        {sectionLinks.map(({ label, section }) => (
-          <div key={section} style={{ display: 'flex', gap: 8 }}>
-            <IconButton type="button">
-              <MdDragIndicator />
-            </IconButton>
-            <StyledLink
-              href={`/generator?section=${section}`}
-              $active={section === currSection}
-            >
-              {label}
-            </StyledLink>
-          </div>
-        ))}
-      </Nav>
-
+      <NavList>
+        <div>
+          {staticSectionLinks.map(({ label, section }) => (
+            <NavItem
+              key={section}
+              label={label}
+              section={section}
+              currSection={currSection}
+            />
+          ))}
+        </div>
+        <DragSort
+          items={sections}
+          onChange={(params) => updateSectionOrder(params)}
+          renderItem={({ label, section }) => {
+            return (
+              <NavItem
+                draggable
+                key={section}
+                label={label}
+                section={section}
+                currSection={currSection}
+              />
+            )
+          }}
+        />
+      </NavList>
       <PrimaryButton form="resume-form">MAKE</PrimaryButton>
     </Aside>
   )
